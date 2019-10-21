82fde38a4c785f4cfc90223d0e4fd1078c08ac50
Elvis Fernandes <elvisfernandesc@gmail.com>
2019-10-20 21:52:16 -0300

    Adicionado auditoria e criação da classe User

---
+++ src/main/java/br/com/security/application/model/AbstractAuditedEntity.java
@@ -0,0 +1,51 @@
+package br.com.security.application.model;
+
+import lombok.Data;
+import org.hibernate.envers.Audited;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * @author Elvis Fernandes on 20/10/19
+ */
+@Data
+@Audited
+@MappedSuperclass
+public class AbstractAuditedEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Version
+    @Column(name = "version", nullable = false)
+    private Long version;
+
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @Column(name = "modified_date")
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "modified_by")
+    private String modifiedBy;
+
+    @Column(name = "active", nullable = false)
+    private Boolean active = Boolean.TRUE;
+}
---
+++ src/main/java/br/com/security/application/model/User.java
@@ -0,0 +1,29 @@
+package br.com.security.application.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * @author Elvis Fernandes on 20/10/19
+ */
+@Entity
+@Table(name = "tb_users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User extends AbstractAuditedEntity {
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+}
